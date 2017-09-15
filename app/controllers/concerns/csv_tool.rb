513e4ff38579068f73add3033107ba6ebd126866
Adam Booth <4rlm@protonmail.ch>
2017-09-15 07:35:02 -0500

    improved CsvToolMod and CsvTool.

--- app/controllers/concerns/csv_tool.rb
+++ app/controllers/concerns/csv_tool.rb
@@ -1,91 +1,21 @@
+# Notes:
+# 1) CsvTool class calls CsvToolMod module.  Both files work together.
+# 2) Note: Ensure config/application.rb extends autoload to concerns.
+
+## Call: CsvTool.new(Account).backup_csv
+## Call: CsvTool.new(Account).download_csv
+
+## Call: CsvTool.new(Account).import_csv
+## Call: CsvTool.new(Account).iterate_csv
+###########################################
+
 require 'csv'
 require 'pry'
 
-module CsvTool
+class CsvTool
   extend ActiveSupport::Concern
-
-  module Export
-    def backup_csv
-      CSV.open(@file_path, "wb") do |csv|
-        csv << @model.attribute_names
-        @model.all.each { |r| csv << r.attributes.values }
-      end
-    end
-
-    def download_csv
-      CSV.generate do |csv|
-        csv << @model.attribute_names
-        @model.all.each { |r| csv << r.attributes.values }
-      end
-    end
-
-  end
-
-  module Import
-
-    def import_csv
-      upload_csv
-      @csv_hashes.each do |valid_hash|
-        begin
-          if obj = @model.find_by(id: valid_hash["id"])
-            @model.record_timestamps = false
-            obj.update_attributes(valid_hash)
-          else
-            @model.record_timestamps = true
-            @model.create!(valid_hash)
-          end
-        rescue
-          puts "\n\nDuplicate Data Error\n\n"
-        end
-
-      end
-    end
-
-    def iterate_csv
-      upload_csv
-      @csv_hashes
-    end
-
-    def upload_csv
-      @csv_hashes = []
-
-      CSV.foreach(@file_path, headers: true, skip_blanks: true) do |row|
-        valid_hash = validate_hash(@model.column_names, row.to_hash)
-        @csv_hashes << valid_hash
-      end
-
-      @csv_hashes
-    end
-
-    def validate_hash(cols, hash)
-      keys = hash.keys
-      keys.each do |key|
-        if !cols.include?(key)
-          hash.delete(key)
-        end
-      end
-      hash
-    end
-
-  end
-
-end
-
-############## Usage / Directions ##############
-# Paste below code into separate file for use, ex: lib/servicers/csv_tool_helper.rb
-
-=begin
-require 'csv'
-require 'pry'
-## Call: CsvToolHelper.new(Account).backup_csv
-## Call: CsvToolHelper.new(Account).download_csv
-
-## CsvToolHelper.new(Account).import_csv
-## CsvToolHelper.new(Account).iterate_csv
-
-class CsvToolHelper
-  include CsvTool::Export
-  include CsvTool::Import
+  include CsvToolMod::Export
+  include CsvToolMod::Import
   attr_reader :file_name, :file_path
 
   def initialize(model)
@@ -95,5 +25,5 @@ class CsvToolHelper
     FileUtils.mkdir_p(@dir_path)
     @file_path = "#{@dir_path}/#{@file_name}"
   end
+
 end
-=end
---
+++ app/controllers/concerns/csv_tool_mod.rb
@@ -0,0 +1,86 @@
+# Notes:
+# 1) CsvTool class calls CsvToolMod module.  Both files work together.
+# 2) Note: Ensure config/application.rb extends autoload to concerns.
+
+## Call: CsvTool.new(Account).backup_csv
+## Call: CsvTool.new(Account).download_csv
+
+## Call: CsvTool.new(Account).import_csv
+## Call: CsvTool.new(Account).iterate_csv
+###########################################
+
+require 'csv'
+require 'pry'
+
+module CsvToolMod
+  extend ActiveSupport::Concern
+
+  module Export
+
+    def backup_csv
+      CSV.open(@file_path, "wb") do |csv|
+        csv << @model.attribute_names
+        @model.all.each { |r| csv << r.attributes.values }
+      end
+    end
+
+    def download_csv
+      CSV.generate do |csv|
+        csv << @model.attribute_names
+        @model.all.each { |r| csv << r.attributes.values }
+      end
+    end
+
+  end
+
+
+  module Import
+
+    def import_csv
+      upload_csv
+      @csv_hashes.each do |valid_hash|
+        begin
+          if obj = @model.find_by(id: valid_hash["id"])
+            @model.record_timestamps = false
+            obj.update_attributes(valid_hash)
+          else
+            @model.record_timestamps = true
+            @model.create!(valid_hash)
+          end
+        rescue
+          puts "\n\nDuplicate Data Error\n\n"
+        end
+
+      end
+    end
+
+    def iterate_csv
+      upload_csv
+      @csv_hashes
+    end
+
+
+    def upload_csv
+      @csv_hashes = []
+
+      CSV.foreach(@file_path, headers: true, skip_blanks: true) do |row|
+        valid_hash = validate_hash(@model.column_names, row.to_hash)
+        @csv_hashes << valid_hash
+      end
+
+      @csv_hashes
+    end
+
+    def validate_hash(cols, hash)
+      keys = hash.keys
+      keys.each do |key|
+        if !cols.include?(key)
+          hash.delete(key)
+        end
+      end
+      hash
+    end
+
+  end
+
+end
